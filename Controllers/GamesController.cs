7da6eff7180df3957ca79eb038cc21f98cad4adc
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2026-09-15 18:52:46 +0300

    Created simple controller for game management with REST API

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GameStore.Api.Models;
+using System.Collections.Concurrent;
+
+
+namespace GameStore.Api.Controllers
+{
+    [ApiController]
+    [Route("api/games")]
+    public class GamesController : ControllerBase
+    {
+        int currId = 0;
+
+        private readonly ConcurrentDictionary<int, Game> _games = new ();
+
+        public record GameDTO(string Name, decimal Price, string Genre);
+
+        public GamesController() { }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Game>> GetAllGames()
+        {
+            return Ok(_games.Values);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Game> GetGameById([FromRoute] int id)
+        {
+            if (_games.TryGetValue(id, out var game))
+            {
+                return Ok(game);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult CreateGame([FromBody] GameDTO reqParams)
+        {
+            int generatedId = Interlocked.Increment(ref currId);
+
+            Game game = new Game { Id = generatedId, Name = reqParams.Name, Price = reqParams.Price, Genre = reqParams.Genre };
+
+            _games[game.Id] = game;
+
+            return CreatedAtAction(nameof(GetGameById),
+                new { id = game.Id },
+                game);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateGame([FromRoute] int id, [FromBody] GameDTO reqParams)
+        {
+            if(_games.TryGetValue(id, out var game))
+            {
+                game.Price = reqParams.Price;
+                game.Name = reqParams.Name;
+                game.Genre = reqParams.Genre;
+
+                return Ok();
+            }
+
+
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteGame([FromRoute] int id)
+        {
+            if(!_games.TryRemove(id, out _))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace GameStore.Api.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        // Use decimal instead of float, to prevent small float precision errors accumulating over time.
+        public decimal Price { get; set; }
+
+        public string Genre { get; set;  } = string.Empty;
+    }
+}
