363ed2638d64eecc61ca1425ff8bedc4dd683bde
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2026-09-15 22:52:22 +0300

    Created an SQL server db

---
+++ Data/Model.cs
@@ -0,0 +1,20 @@
+﻿namespace GameStore.Api.Data
+{
+    using GameStore.Api.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class GameStoreContext : DbContext
+    {
+        public DbSet<Game> Games { get; set;  }
+
+        public GameStoreContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=GameStoreDB;Trusted_Connection=True;");
+        }
+
+    }
+}
