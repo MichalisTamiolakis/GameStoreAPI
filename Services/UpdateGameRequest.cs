7ef9f6be2e33203280c659479d68eaf5366f562c
Michalis Tamiolakis <michalis.tamiolakis@outlook.com>
2026-09-18 12:32:37 +0300

    Added input validation to DTOs

--- Services/UpdateGameRequest.cs
+++ Services/UpdateGameRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace GameStore.Api.Services
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Services
 {
-    public record UpdateGameRequest(string Name, decimal Price, string Genre);
+    public record UpdateGameRequest([Required, StringLength(100, MinimumLength = 1)] string Name, [Required, Range(0.01, 1000)] decimal Price, [Required, StringLength(50, MinimumLength = 1)] string Genre);
 }
